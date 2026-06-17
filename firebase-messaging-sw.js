a36b6423444a8684e9f5f65ca9d759f258bfb56e
cihankurucc79-netizen <cihankurucc79@gmail.com>
2026-06-17 10:23:30 +0300

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,22 +3,9 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-comp
 
 firebase.initializeApp({
     apiKey: "AIzaSyCs4s6f3bRhc8lotVWcEKSUAEH_OQFWR1g",
-    authDomain: "diafon-tetik12.firebaseapp.com",
     projectId: "diafon-tetik12",
-    storageBucket: "diafon-tetik12.firebasestorage.app",
     messagingSenderId: "534092717765",
     appId: "1:534092717765:web:695a90bde7071e207d1aac"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = '📞 Cihan Diafon';
-    const notificationOptions = {
-        body: payload.data?.mesaj || payload.notification?.body || "Yeni bir bildirim!",
-        icon: '/favicon.ico'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
